ad8412a41e0a917fccbd6506c243e22f8a5b828b
iyigames <iyigames.me@gmail.com>
2026-09-23 16:50:18 +0500

    Delete functions directory

--- functions/api/v1/schedule.js
+++
@@ -1,6 +0,0 @@
-// Cloudflare Pages Functions route for /api/v1/schedule
-import { onRequest as scheduleHandler } from '../schedule.js';
-
-export async function onRequest(context) {
-  return scheduleHandler(context);
-}
--- functions/watch/[[catchall]].js
+++
@@ -1,6 +0,0 @@
-// Cloudflare Pages Functions route for /watch/*
-import { onRequest as middlewareHandler } from '../_middleware.js';
-
-export async function onRequest(context) {
-  return middlewareHandler(context);
-}
